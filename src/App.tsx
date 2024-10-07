8a77676b0db943898958acc0be2cd75b02c49d39
Arnaud <arnaudpomponio@yahoo.com>
2024-10-07 17:57:41 +0200

    Add button CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <header style={{ backgroundColor: "rgba(0,0,0,0.3", padding: "2rem" }}>
         <h1 className="titre">PokéFinder</h1>
+        <h2>Hello world</h2>
+        <h3>Hello world</h3>
+        <h4>Hello world</h4>
+        <h5>Hello world</h5>
+        <h6>Hello worldc</h6>
         <p>Lorem ipsum. Test de lecture de l'écran.</p>
+        <a href="/">Cliquez-ici</a>
+        <button className="btn primary">Rechercher</button>
+        <button className="btn secondary">Rechercher</button>
       </header>
     </div>
   );
